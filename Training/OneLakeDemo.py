19b11845c74b977c01fdc8edf75b7f343c4c6459
nawtybean <nawtybean>
2023-12-05 11:16:08 +0000

    some changes

--- Training/OneLakeDemo.py
+++ Training/OneLakeDemo.py
@@ -7,8 +7,11 @@ from contrib.BlobStorageOps import BlobStorage
 SECRET = dbutils.secrets.get(scope="dev", key="Secret")
 TENANT_ID = dbutils.secrets.get(scope="dev", key="TenantId")
 CLIENT_ID = dbutils.secrets.get(scope="dev", key="ClientId")
+BLOB_ACCOUNT_NAME = dbutils.secrets.get(scope="dev", key="BlobAccountName")
+BLOB_PRIMARY_KEY = dbutils.secrets.get(scope="dev", key="BlobPrimaryKey")
+BLOB_CONNECTION_STRING = dbutils.secrets.get(scope="dev", key="BlobConnectionString")
 
-# set the spark context
+# Set the spark context
 spark.conf.set("fs.azure.account.auth.type.medallionzanorth.dfs.core.windows.net", "OAuth")
 spark.conf.set("fs.azure.account.oauth.provider.type.medallionzanorth.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
 spark.conf.set("fs.azure.account.oauth2.client.id.medallionzanorth.dfs.core.windows.net", CLIENT_ID)
@@ -16,9 +19,6 @@ spark.conf.set("fs.azure.account.oauth2.client.secret.medallionzanorth.dfs.core.
 spark.conf.set("fs.azure.account.oauth2.client.endpoint.medallionzanorth.dfs.core.windows.net", "https://login.microsoftonline.com/" + TENANT_ID + "/oauth2/token")
 
 # Setting the constant Variables
-BLOB_ACCOUNT_NAME = dbutils.secrets.get(scope="dev", key="BlobAccountName")
-BLOB_PRIMARY_KEY = dbutils.secrets.get(scope="dev", key="BlobPrimaryKey")
-BLOB_CONNECTION_STRING = dbutils.secrets.get(scope="dev", key="BlobConnectionString")
 ONE_LAKE_URL = "abfss://Dev@onelake.dfs.fabric.microsoft.com/TrainingLakeHouse.Lakehouse"
 CONTAINER = 'bronze'
 
@@ -54,8 +54,4 @@ for i in range(len(blob_lst)):
     print(blob_url + '/' + blob_lst[i])
     df = spark.read.schema(order_schema).csv(blob_url + '/' + blob_lst[i])
     df.write.format("delta").mode("append").save(ONE_LAKE_URL + "/Tables/Orders")
-
-
-# COMMAND ----------
-
 
